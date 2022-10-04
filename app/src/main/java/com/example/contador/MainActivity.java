56177e04f03d3fff28894da5678b81df58f1be7d
ATEIS\a20vanesagp <dona5487@gmail.com>
2022-10-04 18:28:32 +0200

    Clase

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -4,18 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int contador=0;
+
+    TextView tv_pulsado;
+    Button btnLimpiar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contador);
 
+        tv_pulsado = findViewById(R.id.tv_pulsado);
+        btnLimpiar = findViewById(R.id.btn_limpiar);
 
-        findViewById(R.id.button2).setOnClickListener(
-                v -> startActivity(new Intent(this, ContadorActivity.class))
-        );
+        Button btnPulsar = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
 
+        tv_pulsado.setVisibility(View.INVISIBLE);
+
+        btnPulsar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++;
+                if (contador==1) {
+                    textView.setText( "Has pulsado 1 vez");
+                }
+                else if (contador>1){
+                    textView.setText( "Has pulsado "+ contador + " veces");
+                }
+
+                tv_pulsado.setVisibility(View.VISIBLE);
+
+                btnLimpiar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador=0;
+                textView.setText("Contador a 0");
+                tv_pulsado.setVisibility(View.INVISIBLE);
+                btnLimpiar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
